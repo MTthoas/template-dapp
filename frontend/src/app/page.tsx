48c770eaa875ba3419293ce511395b1752e730b7
MTthoas <matthias.pecquery@gmail.com>
2024-08-03 01:58:42 +0200

    fix: front update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,9 @@
-import { GridPattern } from "@/components/ui/grid-pattern";
 import HomePage from "@/containers/home-page";
-import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <main className="h-screen bg-background relative flex size-full items-center justify-center overflow-hidden p-20 md:shadow-xl">
+    <main className="relative z-10">
       <HomePage />
-      <GridPattern
-        width={30}
-        height={30}
-        x={-1}
-        y={-1}
-        strokeDasharray="4 2"
-        className={cn(
-          "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]"
-        )}
-      />
     </main>
   );
 }
--- frontend/src/containers/home-page/index.tsx
+++ frontend/src/containers/home-page/index.tsx
@@ -1,18 +1,29 @@
 "use client";
-import React from "react";
 import { ModeToggle } from "@/components/modeToggle";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col w-1/3">
-      <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white">
-        <span className="font-bold text-6xl">Welcome </span>to the
-        <br />
-        <span className="font-bold underline text-6xl"> MTthoas</span> Template
-      </p>
-      <div className="flex justify-center mt-8">
-        <ModeToggle />
+    <div className="flex flex-col">
+      <div className="w-screen z-1 whitespace-pre-wrap text-5xl font-medium tracking-tighter text-black dark:text-white flex gap-2">
+        <div className="flex-none w-1/2 text-center flex flex-col gap-3">
+          <div className="flex align-center justify-center">
+            <span className="font-bold text-6xl">Basic Dapp Template</span>
+          </div>
+          <span className="font-medium text-3xl">
+            Foundry / NextJS / Express{" "}
+          </span>
+          <div className="flex justify-center mt-4">
+            <ModeToggle />
+          </div>
+        </div>
+        <div className="flex-none w-1/2 text-center">
+          <div className="flex align-center justify-center">
+            <span className="font-bold text-6xl">WAGMI</span>
+          </div>
+          <span className="font-medium text-3xl">Web3 / Ethers / Hardhat</span>
+        </div>
       </div>
+      <div className="flex justify-center mt-8"></div>
     </div>
   );
 }
